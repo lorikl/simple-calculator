7e01f01b3172facbddbc74bc9fde0c7fcb75be0d
Lorik Latifi <Lorikkatifi@gmail.com>
2024-02-29 21:58:02 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let add = document.querySelector('.add')
+let subtract = document.querySelector('.subtract')
+let multiply = document.querySelector('.multiply')
+let divide = document.querySelector('.divide')
+let innertext = document.querySelector('.innerText')
+let numOne = document.querySelector('.numOne').value
+let numTwo = document.querySelector('.numTwo').value
+
+function addition() {
+    return innertext.innerHTML = numOne + numTwo
+}
+
